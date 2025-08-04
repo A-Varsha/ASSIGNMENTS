5737c3c321990413b921fd865012534a54f79f8e
A-Varsha <146081605+A-Varsha@users.noreply.github.com>
2025-08-04 19:49:07 +0530

    Add files via upload

---
+++ verilog assignment/conti_assin.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2025 14:00:27
+// Design Name: 
+// Module Name: conti_assin
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module conti_assin(
+input a,b,
+output out);
+assign out=a & b;
+endmodule
